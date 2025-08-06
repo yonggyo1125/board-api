cc457d141ce000e0598375b6df136ffb0abb4014
yonggyo1125 <yonggyo00@kakao.com>
2025-08-06 15:16:36 +0900

    .

---
+++ src/main/java/org/koreait/global/configs/SecurityConfig.java
@@ -0,0 +1,17 @@
+package org.koreait.global.configs;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.build();
+    }
+}
