55b52e641494e8fe75ab0cb4f209fd23d5683adb
yonggyo1125 <yonggyo00@kakao.com>
2025-08-06 17:03:49 +0900

    .

---
+++ src/main/java/org/koreait/global/exceptions/NotFoundException.java
@@ -0,0 +1,21 @@
+package org.koreait.global.exceptions;
+
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+import java.util.Map;
+
+public class NotFoundException extends CommonException {
+    public NotFoundException() {
+        super("NotFound", HttpStatus.NOT_FOUND);
+        setErrorCode(true);
+    }
+
+    public NotFoundException(String message) {
+        super(message, HttpStatus.NOT_FOUND);
+    }
+
+    public NotFoundException(Map<String, List<String>> errorMessages) {
+        super(errorMessages, HttpStatus.NOT_FOUND);
+    }
+}
---
+++ src/main/java/org/koreait/global/exceptions/UnAuthorizedException.java
@@ -0,0 +1,21 @@
+package org.koreait.global.exceptions;
+
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+import java.util.Map;
+
+public class UnAuthorizedException extends CommonException {
+    public UnAuthorizedException() {
+        super("UnAuthorized", HttpStatus.UNAUTHORIZED);
+        setErrorCode(true);
+    }
+
+    public UnAuthorizedException(String message) {
+        super(message, HttpStatus.UNAUTHORIZED);
+    }
+
+    public UnAuthorizedException(Map<String, List<String>> errorMessages) {
+        super(errorMessages, HttpStatus.UNAUTHORIZED);
+    }
+}
