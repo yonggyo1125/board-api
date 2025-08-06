863d0d796c14e8e7d3c31d33c7376fcc3138635a
yonggyo1125 <yonggyo00@kakao.com>
2025-08-06 16:57:37 +0900

    .

---
+++ src/main/java/org/koreait/global/exceptions/BadRequestException.java
@@ -0,0 +1,21 @@
+package org.koreait.global.exceptions;
+
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+import java.util.Map;
+
+public class BadRequestException extends CommonException {
+    public BadRequestException() {
+        super("BadRequest", HttpStatus.BAD_REQUEST);
+        setErrorCode(true);
+    }
+
+    public BadRequestException(String message) {
+        super(message, HttpStatus.BAD_REQUEST);
+    }
+
+    public BadRequestException(Map<String, List<String>> errorMessages) {
+        super(errorMessages, HttpStatus.BAD_REQUEST);
+    }
+}
