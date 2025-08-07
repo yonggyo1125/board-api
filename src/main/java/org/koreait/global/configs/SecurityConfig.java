bfa004c61b93245123e201a4df91afe287a71808
yonggyo1125 <yonggyo00@kakao.com>
2025-08-07 11:23:24 +0900

    .

--- src/main/java/org/koreait/global/configs/SecurityConfig.java
+++ src/main/java/org/koreait/global/configs/SecurityConfig.java
@@ -14,6 +14,8 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(c -> c.disable());
+
         return http.build();
     }
 
