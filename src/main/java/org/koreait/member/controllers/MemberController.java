f946ef75cdca72e675857ee9ae1f89ce738d2947
yonggyo1125 <yonggyo00@kakao.com>
2025-08-07 11:57:16 +0900

    .

--- src/main/java/org/koreait/member/controllers/MemberController.java
+++ src/main/java/org/koreait/member/controllers/MemberController.java
@@ -1,5 +1,8 @@
 package org.koreait.member.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.koreait.global.exceptions.BadRequestException;
@@ -13,12 +16,14 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/member")
+@Tag(name="회원 API", description = "회원 가입, 회원 인증 토큰 발급 기능 제공")
 public class MemberController {
     private final JoinValidator joinValidator;
     private final JoinService joinService;
     private final Utils utils;
 
-    // 회원 가입
+    @Operation(summary = "회원가입처리", method = "POST")
+    @ApiResponse(responseCode = "201", description = "회원가입 성공시 201로 응답, 검증 실패시 400")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED) // 201로 응답
     public void join(@Valid @RequestBody RequestJoin form, Errors errors) {
---
+++ src/test/java/org/koreait/member/controllers/MemberControllerTest.java
@@ -0,0 +1,45 @@
+package org.koreait.member.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class MemberControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper om;
+
+    @Test
+    @DisplayName("회원가입 컨트롤러 테스트")
+    void joinTest() throws Exception {
+
+        RequestJoin form = new RequestJoin();
+        form.setEmail("user01@test.org");
+        form.setPassword("_aA123456");
+        form.setConfirmPassword(form.getPassword());
+        form.setMobile("01010001000");
+        form.setName("사용자01");
+        form.setTermsAgree(true);
+
+        String body = om.writeValueAsString(form);
+
+        mockMvc.perform(post("/api/v1/member")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(body))
+                .andDo(print())
+                .andExpect(status().isCreated());
+    }
+}
