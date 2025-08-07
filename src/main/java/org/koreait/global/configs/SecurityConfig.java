ec22b584ad833d8b94adca72a9998132b04e01f8
yonggyo1125 <yonggyo00@kakao.com>
2025-08-07 15:25:33 +0900

    .

--- src/main/java/org/koreait/global/configs/SecurityConfig.java
+++ src/main/java/org/koreait/global/configs/SecurityConfig.java
@@ -1,20 +1,25 @@
 package org.koreait.global.configs;
 
 import lombok.RequiredArgsConstructor;
+import org.koreait.member.jwt.LoginFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final LoginFilter loginFilter;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(c -> c.disable());
+        http.csrf(c -> c.disable())
+                .addFilterBefore(loginFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
