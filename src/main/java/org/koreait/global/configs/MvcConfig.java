bb7a201a9a11e714eaf8d19a06eabb2857f2ed42
yonggyo1125 <yonggyo00@kakao.com>
2025-08-07 16:47:33 +0900

    .

--- src/main/java/org/koreait/global/configs/MvcConfig.java
+++ src/main/java/org/koreait/global/configs/MvcConfig.java
@@ -3,10 +3,12 @@ package org.koreait.global.configs;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableJpaAuditing
 @EnableScheduling
+@EnableMethodSecurity
 public class MvcConfig implements WebMvcConfigurer {
 }
--- src/main/java/org/koreait/global/configs/SecurityConfig.java
+++ src/main/java/org/koreait/global/configs/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.koreait.member.jwt.LoginFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -21,6 +22,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(c -> c.disable())
                 .addFilterBefore(loginFilter, UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement(c -> c.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .exceptionHandling(c -> {
                     c.authenticationEntryPoint((req, res, e) -> {
                         res.sendError(HttpServletResponse.SC_UNAUTHORIZED);
@@ -28,6 +30,9 @@ public class SecurityConfig {
                     c.accessDeniedHandler((req, res, e) -> {
                         res.sendError(HttpServletResponse.SC_UNAUTHORIZED);
                     });
+                })
+                .authorizeHttpRequests(c -> {
+                    c.anyRequest().permitAll();
                 });
 
         return http.build();
