9fe0a5fa3a74bcdae16dbaaf4481f05dba96ccf5
yonggyo1125 <yonggyo00@kakao.com>
2025-08-08 15:46:37 +0900

    .

--- src/test/java/org/koreait/file/controllers/FileControllerTest.java
+++ src/test/java/org/koreait/file/controllers/FileControllerTest.java
@@ -1,9 +1,16 @@
 package org.koreait.file.controllers;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.koreait.file.services.FileUploadService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.FileInputStream;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -12,6 +19,18 @@ public class FileControllerTest {
     @Autowired
     private FileUploadService uploadService;
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    private MultipartFile file1;
+    private MultipartFile file2;
+
+    @BeforeEach
+    void init() throws Exception{
+        FileInputStream fis = new FileInputStream("C:/data/1.png");
+        file1 = new MockMultipartFile("file", "image1.png", MediaType.IMAGE_PNG_VALUE, fis);
+        file2 = new MockMultipartFile("file", "image2.png", MediaType.IMAGE_PNG_VALUE, fis);
+    }
 
 }
 
