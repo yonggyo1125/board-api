5a488169cbdc94b84ce83dc78f27b28df4848840
yonggyo1125 <yonggyo00@kakao.com>
2025-08-08 09:45:18 +0900

    .

---
+++ src/main/java/org/koreait/main/controllers/MainController.java
@@ -0,0 +1,12 @@
+package org.koreait.main.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String index() {
+        return "redirect:/api-docs.html";
+    }
+}
