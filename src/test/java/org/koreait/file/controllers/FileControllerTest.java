f4a2681827677f26eaab9598f157d198bc1682c1
yonggyo1125 <yonggyo00@kakao.com>
2025-08-08 15:51:47 +0900

    .

--- src/test/java/org/koreait/file/controllers/FileControllerTest.java
+++ src/test/java/org/koreait/file/controllers/FileControllerTest.java
@@ -1,6 +1,8 @@
 package org.koreait.file.controllers;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.koreait.file.services.FileUploadService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -8,9 +10,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.io.FileInputStream;
+import java.nio.charset.StandardCharsets;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -22,8 +27,8 @@ public class FileControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    private MultipartFile file1;
-    private MultipartFile file2;
+    private MockMultipartFile file1;
+    private MockMultipartFile file2;
 
     @BeforeEach
     void init() throws Exception{
@@ -31,6 +36,21 @@ public class FileControllerTest {
         file1 = new MockMultipartFile("file", "image1.png", MediaType.IMAGE_PNG_VALUE, fis);
         file2 = new MockMultipartFile("file", "image2.png", MediaType.IMAGE_PNG_VALUE, fis);
     }
+    
+    @Test
+    @DisplayName("파일 업로드 테스트")
+    void fileUploadTest() throws Exception {
 
+        String body = mockMvc.perform(multipart("/api/v1/file/upload")
+                .file(file1)
+                .file(file2)
+                .param("gid", "testgid")
+                .param("location", "testLocation"))
+                .andDo(print())
+                .andReturn()
+                .getResponse().getContentAsString(StandardCharsets.UTF_8);
+
+        System.out.println(body);
+    }
 }
 
